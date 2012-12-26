c83efff0fff8d608b4a8222f10b7b25981a2fe11
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2012-12-26 13:42:51 -0200

    removing trailing spaces and bumping version

--- lib/masked_input/rails/version.rb
+++ lib/masked_input/rails/version.rb
@@ -1,8 +1,8 @@
 module MaskedInput
   module Rails
     module Version
-      MAJOR = 0
-      MINOR = 2
+      MAJOR = 1
+      MINOR = 0
       PATCH = 0
       STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
     end
